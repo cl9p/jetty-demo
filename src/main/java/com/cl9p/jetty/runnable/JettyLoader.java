5db3c9f200be9f1e4c921bf358db9fc955c54430
Lee Faus <leefaus@me.com>
2013-01-11 17:56:05 -0500

    initial project (for master)

---
+++ src/main/java/com/cl9p/jetty/runnable/JettyLoader.java
@@ -0,0 +1,11 @@
+package com.cl9p.jetty.runnable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lfaus
+ * Date: 1/11/13
+ * Time: 4:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JettyLoader {
+}
